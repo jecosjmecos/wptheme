843580fbc6f1e7ce5c9bc25095093cde313038f5
Eugene <evgenfree@ukr.net>
2020-11-18 22:39:48 +0200

    wp menu example add

---
+++ wp-menu-example.php
@@ -0,0 +1,19 @@
+<ul class="header-menu">
+	<li class="menu-item menu-item-active menu-item-has-children">
+		<a href="">
+
+		</a>
+		<ul class="sub-menu">
+			<li>
+				<a href="">
+					
+				</a>
+			</li>
+		</ul>
+	</li>
+	<li class="menu-item">
+		<a>
+
+		</a>
+	</li>
+</ul>
